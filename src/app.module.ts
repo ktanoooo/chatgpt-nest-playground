bc873873983d4b11d0bcff4a47d413a495d5c94f
ktanoooo <ktanoooo1112@gmail.com>
2023-07-16 03:41:19 +0900

    Import configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatgptModule } from './chatgpt/chatgpt.module';
 
 @Module({
-  imports: [ChatgptModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ChatgptModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
