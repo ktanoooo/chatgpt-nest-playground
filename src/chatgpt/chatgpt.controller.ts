795018426720d576d215e9216c84e0348b1ab0d6
ktanoooo <ktanoooo1112@gmail.com>
2023-07-16 03:41:55 +0900

    Impl chatgpt sample

--- src/chatgpt/chatgpt.controller.ts
+++ src/chatgpt/chatgpt.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ChatgptService } from './chatgpt.service';
 
 @Controller('chatgpt')
-export class ChatgptController {}
+export class ChatgptController {
+  constructor(private readonly chatgptService: ChatgptService) {}
+
+  @Post()
+  async postMessage(@Body() body: { message: string }) {
+    try {
+      const response = await this.chatgptService.postMessage({
+        message: body.message,
+      });
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
--- src/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Configuration, OpenAIApi } from 'openai';
 
 @Injectable()
-export class ChatgptService {}
+export class ChatgptService {
+  private openai: OpenAIApi;
+
+  constructor(private readonly configService: ConfigService) {
+    const configuration = new Configuration({
+      apiKey: this.configService.get<string>('OPENAI_API_KEY'),
+    });
+    this.openai = new OpenAIApi(configuration);
+  }
+
+  async postMessage({ message }: { message: string }) {
+    try {
+      console.log(message);
+      const completion = await this.openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: message }],
+      });
+      console.log(completion.data.choices);
+    } catch (e) {
+      console.log(e);
+      return e;
+    }
+  }
+}
